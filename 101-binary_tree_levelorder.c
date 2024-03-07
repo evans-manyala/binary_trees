3d40caa96249481fbb795330f909294436ebdb33
Evans Manyala <evans.manyala@gmail.com>
2024-03-07 12:19:14 +0300

    Fixing Memory leak errors

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,4 +1,28 @@
 #include "binary_trees.h"
+
+/**
+ * enqueue - Helper function that Enqueues a node in the queue.
+ * @queue: The queue.
+ * @rear: The rear index of the queue.
+ * @size: The size of the queue.
+ * @node: The node to enqueue.
+ */
+static void enqueue(binary_tree_t **queue, int *rear, int *size,
+		    binary_tree_t *node)
+{
+	if (*rear >= *size)
+	{
+		*size *= 2;
+		queue = realloc(queue, sizeof(binary_tree_t *) * (*size));
+		if (queue == NULL)
+		{
+			free(queue);
+			exit(EXIT_FAILURE);
+		}
+	}
+	queue[(*rear)++] = node;
+}
+
 /**
  * binary_tree_levelorder - Function traverses a binary tree
  * via level-order traversal
@@ -6,20 +30,20 @@
  * @func: pointer to a function to call for each node.
  * If tree or func is NULL, do nothing
  */
-
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	binary_tree_t **queue;
-	int front, rear;
+	int front = 0, rear = 0, size = 1;
 
 	if (tree == NULL || func == NULL)
 		return;
 
-	queue = malloc(sizeof(binary_tree_t *));
-	front = 0;
-	rear = 1;
+	queue = malloc(sizeof(binary_tree_t *) * size);
 
-	queue[0] = (binary_tree_t *)tree;
+	if (queue == NULL)
+		return;
+
+	enqueue(queue, &rear, &size, (binary_tree_t *)tree);
 
 	/* Perform level-order traversal */
 	while (front < rear)
@@ -28,15 +52,10 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 
 		func(current->n);
 		if (current->left != NULL)
-		{
-			queue = realloc(queue, sizeof(binary_tree_t *) * (rear++));
-			queue[rear++] = current->left;
-		}
+			enqueue(queue, &rear, &size, current->left);
 		if (current->right != NULL)
-		{
-			queue = realloc(queue, sizeof(binary_tree_t *) * (rear++));
-			queue[rear++] = current->right;
-		}
+			enqueue(queue, &rear, &size, current->right);
+
 		front++;
 	}
 	free(queue);
