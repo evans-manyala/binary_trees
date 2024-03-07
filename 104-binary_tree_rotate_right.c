9cb05131d06980de04fadce240ade38feb71abc3
Evans Manyala <evans.manyala@gmail.com>
2024-03-07 20:43:02 +0300

    Adding code logic fix

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,18 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_right - Performs a left-rotation on a binary tree.
+ * binary_tree_rotate_right - Performs a right-rotation on a binary tree.
  * @tree: Pointer to the root node of the tree to rotate.
  * Return: Pointer to the new root node of the tree once rotated.
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *new_root;
-
 	if (tree == NULL || tree->left == NULL)
-		return (tree);
+		return tree;
 
-	new_root = tree->left;
+	binary_tree_t *new_root = tree->left;
 	tree->left = new_root->right;
 
 	if (new_root->right != NULL)
@@ -28,8 +26,8 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 			tree->parent->right = new_root;
 	}
 
-	new_root->left = tree;
+	new_root->right = tree;
 	tree->parent = new_root;
 
-	return (new_root);
+	return new_root;
 }
