728c9a56178ddd72dcb67a971ccc4fd6ee074c9c
Evans Manyala <36915027+evans-manyala@users.noreply.github.com>
2024-03-09 13:13:18 +0300

    Update 112-array_to_bst.c

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * array_to_bst - Converts an array of integers into a Binary Search Tree (BST).
+ * array_to_bst - Function xonverts an array of integers
+ * into a Binary Search Tree (BST).
  * @array: The array of integers to be converted.
  * @size: The size of the array.
  * Return: A pointer to the root node of the BST, or NULL on failure.
@@ -12,6 +13,7 @@ bst_t *array_to_bst(int *array, size_t size)
 		return (NULL);
 
 	bst_t *tree = NULL;
+
 	for (size_t i = 0; i < size; i++)
 	{
 		bst_insert(&tree, array[i]);
