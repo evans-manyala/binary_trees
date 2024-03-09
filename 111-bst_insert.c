7f13d106d6b087699dd6705758cbd1533835a97b
Evans Manyala <36915027+evans-manyala@users.noreply.github.com>
2024-03-09 12:52:37 +0300

    Adding code logic

---
+++ 111-bst_insert.c
@@ -0,0 +1,55 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - Inserts a value into a Binary Search Tree (BST).
+ *
+ * @tree: Double pointer to the root node of the BST to insert the value.
+ * @value: The value to store in the node to be inserted.
+ *
+ * Return: A pointer to the created node, or NULL on failure.
+ *
+ * Description:
+ * - If the address stored in tree is NULL, the created node must become the root node.
+ * - If the value is already present in the tree, it must be ignored.
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	if (tree == NULL)
+	{
+		return (NULL);
+	}
+
+	if (*tree == NULL)
+	{
+		bst_t *new_node = binary_tree_node(NULL, value);
+		*tree = new_node;
+		return (new_node);
+	}
+
+		bst_t *current_node = *tree;
+	while (1)
+	{
+		if (value < current_node->n)
+		{
+			if (current_node->left == NULL)
+			{
+				bst_t *new_node = binary_tree_node(current_node, value);
+				current_node->left = new_node;
+				return (new_node);
+			}
+			current_node = current_node->left;
+		}
+		else if (value > current_node->n)
+		{
+			if (current_node->right == NULL)
+			{
+				bst_t *new_node = binary_tree_node(current_node, value);
+				current_node->right = new_node;
+				return (new_node);
+			}
+			current_node = current_node->right;
+		}
+		else
+			return (NULL);
+	}
+}
